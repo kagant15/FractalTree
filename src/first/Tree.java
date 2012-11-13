ed5509912e77ba03e9ba21cb94edb19c1a799d32
Tom Kagan <kagant15@uww.edu>
2012-11-13 14:20:30 -0600

    added comments and removed LineDraw class

--- src/first/Tree.java
+++ src/first/Tree.java
@@ -1,7 +1,5 @@
 package first;
 
-
-
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
--- src/first/View.java
+++ src/first/View.java
@@ -12,67 +12,85 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class View extends JPanel{
 	
-	Color[] colors = new Color[6];
-
- 	Stroke drawingStroke = new BasicStroke(12);
+	Color[] colors = new Color[6];				//Array of colors for branches
+ 	Stroke drawingStroke = new BasicStroke(12); //Line thickness
  	
 	public void paint(Graphics g)
     {
-      super.paint(g);
-
       Graphics2D panel = (Graphics2D) g;
-      panel.setStroke(drawingStroke);
+      
       //HANDLES RENDERING --- MAKES THE PICTURE LOOK NICE, SMOTHER AND MORE FLUSH
       RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-      rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+      				 rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
       panel.setRenderingHints(rh);
- 
-	  	 colors[0]=Color.RED;
-	  	 colors[1]=Color.BLUE;
-	  	 colors[2]=Color.GRAY;
-	  	 colors[3]=Color.YELLOW;
-	  	 colors[4]=Color.BLACK;
-	  	 colors[5]=Color.ORANGE;
       
-	  panel.drawLine(250, 380, 250, 300);	 
-      makeTree(panel, 4, 4, 250, 300);
+      //Set the thickness of the stroke
+      panel.setStroke(drawingStroke);
       
-    }
+	  colors[0]=Color.RED;
+	  colors[1]=Color.BLUE;
+	  colors[2]=Color.GRAY;
+	  colors[3]=Color.YELLOW;
+	  colors[4]=Color.BLACK;
+	  colors[5]=Color.ORANGE;
+      
+	  //Draw the trunk
+	  panel.drawLine(250, 380, 250, 300);
+	  
+	  //Makes the fractal tree
+      makeTree(3, 3, 250, 300, panel);
+      
+    }//End of paint method
 	
-    public void makeTree(Graphics2D panel, int B, int level, int x1, int y1){
+	/**
+	 * This is the fractal algorithm that generates the branches of a tree.
+	 * @param B		The number of branches at each level
+	 * @param level	The number branch levels.  The number of generations
+	 * @param x1	The starting X coordinate
+	 * @param y1	The starting Y coordinate
+	 * @param panel The graphics 2D object that the lines are to be drawn to
+	 */
+    public void makeTree(int B, int level, int x1, int y1, Graphics2D panel){
     	if(level==0){
     		return;
     	}
-    	int c=1;
-    		
-    	double theta=Math.PI/B;
-    	double length=Math.random()*(level * 2)+60;
-    
-    	//make the branches
-    	for(int i=0; i<=B-1; i++){
-    	 double beta=theta*i;
-    	 
-    	 int x2=(int) (x1+length*Math.cos(Math.random()*theta+beta));
-    	 int y2=(int) (y1-length*Math.sin(Math.random()*theta+beta));
-    	 
-    	 if(c==1){
-    		 panel.setColor(colors[1]);//blue
-    		 c=2;
-    	 }
-    	 else if(c==2){
-    		 panel.setColor(colors[2]);//grey
-    		 c=3;
-    	 }
-    	 else{
-    		 panel.setColor(colors[4]);//black
-    		 c=1;
-    	 }
-    	 Stroke drawingStroke = new BasicStroke(level*2);
-    	 panel.setStroke(drawingStroke);
-    	 panel.drawLine(x1, y1, x2, y2);
     	
-    	 makeTree(panel, B, level-1, x2, y2);
+    	//The angle range between the branch and the horizon
+    	double theta=Math.PI/B;
+    	
+    	//The length of the branch
+    	double length=Math.random()*(level*2)+(20*level);
+    
+    	//Make the branches
+    	for(int branchNumber=0; branchNumber<B; branchNumber++){
+    		double beta=theta*branchNumber;
+    	 
+    		//Calculates the ending X and Y coordinates for the branch
+    		int x2=(int) (x1+length*Math.cos(Math.random()*theta+beta));
+    		int y2=(int) (y1-length*Math.sin(Math.random()*theta+beta));
+    	 
+    		//Determine and set the color of the branch
+    		if(branchNumber==0){
+    			panel.setColor(colors[1]);//blue
+    		}
+    		else if(branchNumber==1){
+    			panel.setColor(colors[2]);//grey
+    		}
+    		else{
+    			panel.setColor(colors[4]);//black
+    		}
+    		
+    		//Set thickness of the branch
+    		Stroke drawingStroke = new BasicStroke(level*2);
+    	 	panel.setStroke(drawingStroke);
+    	 
+    	 	//draw line
+    	 	panel.drawLine(x1, y1, x2, y2);
+    	
+    	 	//Recursive call
+    	 	makeTree(B, level-1, x2, y2, panel);
     	}
+    	
    }//end of makeTree()
 	
 	
